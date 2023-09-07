0f4bef3699cb482f45068b4aa873db8ad8604231
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2023-09-07 17:20:22 -0300

    Add handler for POST

---
+++ src/rest/folder/:param/post.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+const handler = (req: Request, res: Response) => {
+  return res.status(200).json({ message: "Test POST responde" });
+};
+
+export default handler;
