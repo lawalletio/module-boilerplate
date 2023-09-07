d548db083707e0c27ce08a9354959287918df025
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2023-09-07 19:17:57 -0300

    Apply lint / format

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,9 +1,9 @@
-import request from "supertest";
-import app from "../src/app";
+import request from 'supertest';
+import app from '../src/app';
 
-describe("Api request", () => {
-  it("should return status 200", async () => {
-    const res = await request(app).get("/folder/testparam1");
+describe('Api request', () => {
+  it('should return status 200', async () => {
+    const res = await request(app).get('/folder/testparam1');
     expect(res.statusCode).toEqual(200);
   });
 });
