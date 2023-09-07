f7219ab93d53a57a6e9d7c836fa235f28240e0ea
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2023-09-07 17:21:48 -0300

    Refactor router building util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,41 +1,31 @@
 import { Router } from "express";
-import fs from "fs";
+import { globSync } from "glob";
+
 import path from "path";
-import { fileURLToPath } from "url";
 
 type RouteMethod = "get" | "post" | "put" | "patch" | "delete";
 
 export const generateRoutes = (relativePath: string): Router => {
   const router = Router();
-  const files = getAllFiles(relativePath);
 
-  // Iterate through each file in the directory
-  files.forEach(async (filePath) => {
-    const routeMethod = path
-      .basename(filePath, path.extname(filePath))
-      .toLowerCase();
+  globSync(`*.js`, {
+    withFileTypes: true,
+    cwd: relativePath,
+    matchBase: true,
+    nocase: true,
+    nodir: true,
+  }).forEach(async (value) => {
+    const filePath = value.relative();
+    const matches = filePath.match(/(get|post|put|patch|delete)\.js/i);
 
-    if (!/([get|post|put|patch|delete]\.js)$/.test(filePath)) {
+    if (matches) {
+      const method: RouteMethod = matches[1] as RouteMethod;
+
+      router[method](filePath, (await require(value.fullpath())).default);
+      console.info(`Created ${method.toUpperCase()} route for ${filePath}`);
+    } else {
       console.warn(
         `Skipping ${filePath} as it doesn't correspond to a valid HTTP method.`,
-      );
-    } else {
-      // Create a new Express route based on the filename
-      console.info("-----------");
-
-      console.info("filePath:");
-      console.info(filePath);
-      console.info("routeMethod:");
-      console.info(routeMethod);
-      console.info("-----------");
-
-      router[routeMethod as RouteMethod](
-        filePath,
-        (await import(filePath)).default,
-      );
-
-      console.info(
-        `Created ${routeMethod.toUpperCase()} route for ${filePath}`,
       );
     }
   });
@@ -44,28 +34,5 @@ export const generateRoutes = (relativePath: string): Router => {
 };
 
 export const getRestDirectory = (relativePath: string = "rest"): string => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
   return path.join(__dirname, relativePath);
-};
-
-export const getAllFiles = (
-  dirPath: string,
-  arrayOfFiles: string[] = [],
-): string[] => {
-  let files = fs.readdirSync(dirPath);
-  files = files.filter((path) => !/\.map$/.test(path));
-
-  arrayOfFiles = arrayOfFiles || [];
-
-  files.forEach((file) => {
-    if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
-      arrayOfFiles = getAllFiles(path.join(dirPath, file), arrayOfFiles);
-    } else {
-      arrayOfFiles.push(path.join(dirPath, file));
-    }
-  });
-
-  return arrayOfFiles;
 };
