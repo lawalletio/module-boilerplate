e5b35c476e549b48f25df088b0e809145a114f78
Agustin Kassis <webmaster@masize.com>
2023-09-11 17:13:56 -0300

    added Outbox implementation

--- src/services/outbox/Outbox.ts
+++ src/services/outbox/Outbox.ts
@@ -1,9 +1,23 @@
+import NDK, {
+  NDKEvent,
+  NDKPrivateKeySigner,
+  NostrEvent,
+} from '@nostr-dev-kit/ndk';
+import relayList from '@constants/relays.json';
+
 export class OutboxService {
+  private ndk: NDK;
+
   constructor() {
-    console.info('This is the user service!');
+    this.ndk = new NDK({
+      explicitRelayUrls: relayList,
+      signer: new NDKPrivateKeySigner(process.env.NOSTR_PRIVATE_KEY),
+    });
   }
 
-  publish() {
+  publish(event: NostrEvent) {
+    const ndkEvent = new NDKEvent(this.ndk, event);
+    ndkEvent.publish();
     return 'Published Outbox!';
   }
 }
