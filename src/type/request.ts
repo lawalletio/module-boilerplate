89f62853678af39fff330f8424b58bf0649f3d1a
Agustin Kassis <webmaster@masize.com>
2023-09-11 17:14:08 -0300

    include outbox to request context

--- src/type/request.ts
+++ src/type/request.ts
@@ -1,8 +1,10 @@
 import { Request } from 'express';
 import type { PrismaClient } from '@prisma/client';
+import { OutboxService } from '@services/outbox/Outbox';
 
 export interface Context {
   prisma?: PrismaClient;
+  outbox: OutboxService;
 }
 
 export interface ExtendedRequest extends Request {
