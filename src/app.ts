aa235948459de57291be1c9d431e494da1e83f08
Agustin Kassis <webmaster@masize.com>
2023-09-12 17:28:55 -0300

    refactored app

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,18 @@
 import './lib/websockets';
-import NDK, { NDKRelay } from '@nostr-dev-kit/ndk';
 
-import relayList from './constants/relays.json';
-import { PrismaClient } from '@prisma/client';
 import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 import * as middlewares from './lib/middlewares';
-import { setUpRoutes, setUpSubscriptions } from './lib/utils';
-import path from 'path';
-import { ExtendedRequest } from './type/request';
-import { OutboxService } from './services/outbox/Outbox';
 
 // Instantiate .env
 dotenv.config({ path: __dirname + '/.env' });
 
-// Instantiate prisma client
-console.info('Instantiate prisma');
-const prisma = new PrismaClient({
-  log: [{ level: 'query', emit: 'event' }],
-});
-
 // Instantiate expresss
 console.info('Instantiate express');
 const app = express();
-
-// Instantiate ndk
-console.info('Instantiate NDK');
-const ndk = new NDK({
-  explicitRelayUrls: relayList,
-});
-
-// --------- //
 
 // Header Middleware
 app.use(morgan('dev'));
@@ -41,46 +20,8 @@ app.use(helmet());
 app.use(express.json());
 app.use(cors());
 
-// Generate routes
-console.info('Setting up routes...');
-const routes = setUpRoutes(express.Router(), path.join(__dirname, 'rest'));
-
-if (null === routes) {
-  throw new Error('Error setting up routes');
-}
-
-// Setup context
-routes.use((req, res, next) => {
-  console.log('Time:', Date.now());
-  (req as ExtendedRequest).context = {
-    prisma,
-    outbox: new OutboxService(),
-  };
-  next();
-});
-
 // Setup express routes
-app.use('/', routes);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
-
-// Connect to Nostr
-console.info('Connecting to Nostr...');
-ndk.connect();
-
-console.info('Subscribing...');
-const subscribed = setUpSubscriptions(ndk, path.join(__dirname, 'nostr'));
-
-if (null === subscribed) {
-  throw new Error('Error setting up subscriptions');
-}
-
-ndk.pool.on('relay:connect', (relay: NDKRelay) => {
-  console.info('Connected to Relay', relay.url);
-});
-
-ndk.on('error', () => {
-  console.info('Error connecting to Relay');
-});
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,70 @@
+import NDK, { type NDKRelay } from '@nostr-dev-kit/ndk';
 import app from './app';
+import path from 'path';
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { setUpRoutes, setUpSubscriptions } from '@lib/utils';
+import { OutboxService } from '@services/outbox/Outbox';
+import { ExtendedRequest } from '@type/request';
+import relayList from './constants/relays.json';
 
 const port = process.env.PORT || 8000;
 
+// Instantiate prisma client
+console.info('Instantiate prisma');
+const prisma = new PrismaClient({
+  log: [{ level: 'query', emit: 'event' }],
+});
+
+// Instantiate ndk
+console.info('Instantiate NDK');
+const ndk = new NDK({
+  explicitRelayUrls: relayList,
+});
+
+console.info('Subscribing...');
+const subscribed = setUpSubscriptions(ndk, path.join(__dirname, 'nostr'));
+
+if (null === subscribed) {
+  throw new Error('Error setting up subscriptions');
+}
+
+ndk.pool.on('relay:connect', (relay: NDKRelay) => {
+  console.info('Connected to Relay', relay.url);
+});
+
+ndk.on('error', () => {
+  console.info('Error connecting to Relay');
+});
+
+// Generate routes
+console.info('Setting up routes...');
+const routes = setUpRoutes(express.Router(), path.join(__dirname, 'rest'));
+
+if (null === routes) {
+  throw new Error('Error setting up routes');
+}
+
+// Setup context
+routes.use((req, res, next) => {
+  console.log('Time:', Date.now());
+  (req as ExtendedRequest).context = {
+    prisma,
+    outbox: new OutboxService(),
+  };
+  next();
+});
+
+// Setup express routes
+app.use('/', routes);
+
+//-- Start process --//
+
+// Start listening
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Connect to Nostr
+console.info('Connecting to Nostr...');
+ndk.connect();
