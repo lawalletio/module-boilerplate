b969b8f9f8369926b591fa1bae875a973821cab0
Agustin Kassis <webmaster@masize.com>
2023-09-13 19:53:46 -0300

    added param

--- src/rest/folder/:param/get.ts
+++ src/rest/folder/:param/get.ts
@@ -2,6 +2,7 @@ import type { Response } from 'express';
 import type { ExtendedRequest } from '@type/request';
 
 const handler = (req: ExtendedRequest, res: Response) => {
+  console.dir(req.params);
   return res.status(200).json({ message: 'Test GET responde' });
 };
 
