7aa74353413eb2f902c4421dca119f9807a30249
Juan José Ramírez <artjjrn@gmail.com>
2024-01-16 12:38:33 -0300

    Configure unit testing

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,33 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  testMatch: [
-    '<rootDir>/**/(*.)test.(js|ts)',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
-  ],
-  testEnvironment: 'node',
   clearMocks: true,
   collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+  ],
   coverageDirectory: '.coverage',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  coverageProvider: 'v8',
+  moduleNameMapper: {
+    "^@constants/(.*)$": '<rootDir>/src/constants/$1',
+    "^@lib/(.*)$": '<rootDir>/src/lib/$1',
+    "^@nostr/(.*)$": '<rootDir>/src/nostr/$1',
+    "^@rest/(.*)$": '<rootDir>/src/rest/$1',
+    "^@services/(.*)$": '<rootDir>/src/services/$1',
+    "^@type/(.*)$": '<rootDir>/src/type/$1',
+  },
+  randomize: true,
+  setupFiles: [
+    'dotenv/config',
+  ],
+  testEnvironment: 'node',
+  testMatch: [
+    '<rootDir>/tests/**/*.test.ts',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+  ],
+  transform: {
+    '\\.ts$': '@swc/jest',
+  },
 };
