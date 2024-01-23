e6c2944995f193b87649061071c17c072840bb49
Juan José Ramírez <artjjrn@gmail.com>
2024-01-23 12:09:32 -0300

    Unit test ndk wrapper

--- __mocks__/@nostr-dev-kit/ndk.ts
+++ __mocks__/@nostr-dev-kit/ndk.ts
@@ -1,6 +1,20 @@
 export const mockEventPublish = jest.fn();
+
 export const NDKEvent = jest.fn().mockImplementation(() => {
   return {
     publish: mockEventPublish,
   }
 });
+
+export const NDKPrivateKeySigner = jest.fn();
+
+export const mockedNDKRelay = {
+    connect: jest.fn().mockResolvedValue(''),
+    disconnect: jest.fn(),
+    on: jest.fn(),
+  };
+export const NDKRelay = jest.fn(() => mockedNDKRelay);
+
+export const NDKRelaySet = jest.fn();
+
+export default jest.fn();
---
+++ tests/services/ndk.test.ts
@@ -0,0 +1,84 @@
+import NDK, { NDKRelay } from '@nostr-dev-kit/ndk';
+import {
+  connectToTempRelays,
+  getReadNDK,
+  getSignerNDK,
+  getWriteNDK,
+} from '@services/ndk';
+import { mockedNDKRelay } from '../../__mocks__/@nostr-dev-kit/ndk';
+
+const now: number = 1231006505000;
+jest.useFakeTimers({ now });
+
+describe('NDK wrapper', () => {
+  describe('getReadNDK', () => {
+    it('should create the ndk only once', () => {
+      const readNDK = getReadNDK();
+
+      expect(getReadNDK()).toBe(readNDK);
+      expect(NDK).toHaveBeenCalledTimes(1);
+      expect(NDK).toHaveBeenCalledWith({
+        explicitRelayUrls: [
+          'wss://relay.lawallet.ar',
+          'wss://relay.example.org',
+        ],
+      });
+    });
+  });
+
+  describe('getWriteNDK', () => {
+    it('should create the ndk only once', () => {
+      const writeNDK = getWriteNDK();
+
+      expect(getWriteNDK()).toBe(writeNDK);
+      expect(NDK).toHaveBeenCalledTimes(1);
+      expect(NDK).toHaveBeenCalledWith(
+        expect.objectContaining({
+          explicitRelayUrls: ['wss://write.relay.lawallet.ar'],
+        }),
+      );
+    });
+  });
+
+  describe('getSignerNDK', () => {
+    it('should give an ndk instance', () => {
+      getSignerNDK();
+      expect(NDK).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('connectToTempRelays', () => {
+    beforeEach(() => {
+      jest.clearAllTimers();
+    });
+    it('should', () => {
+      let relays: string[] = ['wss://relay.lawallet.ar'];
+      const ndk = new NDK();
+      mockedNDKRelay.on.mockImplementationOnce((_, fn) => {
+        fn();
+      });
+
+      connectToTempRelays(relays, ndk);
+
+      expect(NDKRelay).toHaveBeenCalledTimes(1);
+      expect(NDKRelay).toHaveBeenCalledWith('wss://relay.lawallet.ar');
+
+      jest.advanceTimersByTime(5 * 60 * 1000);
+      jest.mocked(NDKRelay).mockClear();
+      relays.push('wss://relay.example.org');
+
+      connectToTempRelays(relays, ndk);
+
+      expect(NDKRelay).toHaveBeenCalledTimes(1);
+      expect(NDKRelay).toHaveBeenCalledWith('wss://relay.example.org');
+
+      jest.advanceTimersByTime(10 * 60 * 1000);
+      jest.mocked(NDKRelay).mockClear();
+      mockedNDKRelay.connect.mockRejectedValueOnce('Oops');
+
+      connectToTempRelays(relays, ndk);
+
+      expect(NDKRelay).toHaveBeenCalledTimes(2);
+    });
+  });
+});
