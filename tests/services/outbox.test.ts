f262fdcdca777d4e19fa69a4df0d5598977879c1
Juan José Ramírez <artjjrn@gmail.com>
2024-01-23 16:46:45 -0300

    Unit test outbox

---
+++ tests/services/outbox.test.ts
@@ -0,0 +1,35 @@
+import NDK, { NostrEvent } from '@nostr-dev-kit/ndk';
+import { OutboxService } from '@services/outbox';
+import { mockEventPublish } from '../../__mocks__/@nostr-dev-kit/ndk';
+
+describe('Outbox service', () => {
+  const ndk = new NDK();
+  const outbox = new OutboxService(ndk);
+  it('should publish an event correctly', async () => {
+    expect.assertions(2);
+    mockEventPublish.mockResolvedValueOnce({size: 1});
+
+    await expect(outbox.publish({} as NostrEvent)).resolves.not.toThrow();
+    expect(mockEventPublish).toHaveBeenCalled();
+  });
+
+  it('should fail if no relay published', async () => {
+    expect.assertions(2);
+    mockEventPublish.mockResolvedValueOnce({size: 0});
+
+    await expect(outbox.publish({} as NostrEvent)).rejects.toEqual(
+      'Did not publish to any relay',
+    );
+    expect(mockEventPublish).toHaveBeenCalled();
+  });
+
+  it('should fail if publishing failed', async () => {
+    expect.assertions(2);
+    mockEventPublish.mockRejectedValueOnce('');
+
+    await expect(outbox.publish({} as NostrEvent)).rejects.toEqual(
+      'Unexpected error publishing event',
+    );
+    expect(mockEventPublish).toHaveBeenCalled();
+  });
+});
